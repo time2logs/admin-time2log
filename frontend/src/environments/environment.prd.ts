1f8df574988ce0b4d2cf0abfe6c8694d646c9493
JoenArnet <joenarnet@bbv.ch>
2026-03-17 08:29:11 +0100

    Change Supabase URL and key in production config
    Updated Supabase configuration for production environment. Um zu testen ob dies den fehler beheben würde

--- frontend/src/environments/environment.prd.ts
+++ frontend/src/environments/environment.prd.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: '/api',
-  supabaseUrl: 'https://rthgesnneqwvzctganyz.supabase.co',
-  supabaseKey: 'sb_publishable_DsFsBT9lbEOWaevZL3EkJw_48dtJ2Ur',
+  supabaseUrl: 'https://icakwycwpldldvcyhpqe.supabase.co',
+  supabaseKey: 'sb_publishable_e8-f5Xz1f9qimbSb1kaQ5w_1sAAGvnh',
 };
