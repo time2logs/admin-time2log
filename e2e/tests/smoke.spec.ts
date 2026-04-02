776107ce5e423d65565ac2c0c1e7313977be58a5
LukasTraut <lukastraut@hotmail.com>
2026-04-02 11:53:17 +0200

    Cancel Invites and remove Member tests

--- e2e/tests/smoke.spec.ts
+++ e2e/tests/smoke.spec.ts
@@ -100,7 +100,63 @@ test.describe('Smoke Tests', () => {
     await expect(page).toHaveURL('/organizations');
     await expect(page.getByTestId('organization-name-input')).toBeVisible();
   })
+
+
+
+  test('invite cancel', async ({ page }) => {
+    await page.goto('/auth/login');
+    await page.fill('#email', 'testusertime2log@gmail.com');
+    await page.fill('#password', 'TestUserBLJT2L');
+    await page.click('button[type="submit"]');
+    await expect(page).toHaveURL('/dashboard');
+
+    await page.goto('/organizations');
+    await page.getByTestId('organization-name-input').fill('TestDeleteOrg');
+    await page.getByTestId('create-organization-button').click();
+    await expect(page.getByText('TestDeleteOrg')).toBeVisible();
+
+    await page.getByText('TestDeleteOrg').click();
+    await page.getByTestId('invite-member-button').click();
+    await page.getByTestId('email-invite-button').fill('Test@test.com');
+    await page.getByTestId('invite-confirm-button').click();
+
+    await page.getByTestId('back-button').click();
+    await expect(page).toHaveURL('/organizations');
+
+    await page.getByText('TestDeleteOrg').click();
+    await page.getByTestId('cancel-invite-button').click();
+
+    await page.getByTestId('open-settings-button').click();
+    await page.getByTestId('delete-organization-button').click();
+    await page.getByTestId('confirm-delete-organization-button').click();
+    await expect(page).toHaveURL('/organizations');
+  })
+
+  test('remove Member', async ({ page }) => {
+    await page.goto('/auth/login');
+    await page.fill('#email', 'testusertime2log@gmail.com');
+    await page.fill('#password', 'TestUserBLJT2L');
+    await page.click('button[type="submit"]');
+    await expect(page).toHaveURL('/dashboard');
+
+    await page.goto('/organizations');
+    await page.getByText('RemoveMember').click();
+
+    const lukasRow = page.locator('li', { hasText: 'Lukas Traut' });
+    await lukasRow.hover();
+
+    await lukasRow.getByTestId('remove-member-button').click();
+    await lukasRow.getByTestId('confirm-remove-member-button').click();
+
+    await page.getByTestId('open-settings-button').click();
+    await page.getByTestId('delete-organization-button').click();
+    await page.getByTestId('confirm-delete-organization-button').click();
+    await expect(page).toHaveURL('/organizations');
+  });
 });
+
+
+
 
 
 
