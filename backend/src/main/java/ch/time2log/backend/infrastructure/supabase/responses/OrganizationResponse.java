65117b01b5b518e1c35c4d5c087cb889a4a87878
LukasTraut <lukastraut@hotmail.com>
2026-04-10 10:52:59 +0200

    erster test remove button auf dem dev branch

--- backend/src/main/java/ch/time2log/backend/infrastructure/supabase/responses/OrganizationResponse.java
+++ backend/src/main/java/ch/time2log/backend/infrastructure/supabase/responses/OrganizationResponse.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 public record OrganizationResponse(
         UUID id,
         String name,
+        UUID created_by,
         OffsetDateTime created_at,
         OffsetDateTime updated_at
 ) {}
