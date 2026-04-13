3db27c3b512dacdd7918dfda67c9216dd714d4a3
Akij <user.of.replit@gmail.com>
2026-04-13 14:08:39 +0200

    fix Dockerfile and environment files. change coolify to use two domains for backend and frontend (domain) and add variable for API_BASE_URL

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,6 +1,8 @@
+declare const ENV_API_BASE_URL: string;
+
 export const environment = {
   production: false,
-  apiBaseUrl: '/api',
+  apiBaseUrl: ENV_API_BASE_URL + '/api',
   supabaseUrl: 'https://icakwycwpldldvcyhpqe.supabase.co',
   supabaseKey: 'sb_publishable_e8-f5Xz1f9qimbSb1kaQ5w_1sAAGvnh',
 };
--- frontend/src/environments/environment.prd.ts
+++ frontend/src/environments/environment.prd.ts
@@ -1,6 +1,8 @@
+declare const ENV_API_BASE_URL: string;
+
 export const environment = {
   production: true,
-  apiBaseUrl: '/api',
+  apiBaseUrl: ENV_API_BASE_URL + '/api',
   supabaseUrl: 'https://rthgesnneqwvzctganyz.supabase.co',
   supabaseKey: 'sb_publishable_DsFsBT9lbEOWaevZL3EkJw_48dtJ2Ur',
 };
