e64b20f3fa4b1b4a2ddc1cd5e85a989927540eb1
LukasTraut <lukastraut@hotmail.com>
2026-06-02 11:00:04 +0200

    delete the import "of"

--- frontend/src/app/core/services/report.service.ts
+++ frontend/src/app/core/services/report.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import { environment } from '@env/environment';
 import {
   ActivitySummary,
